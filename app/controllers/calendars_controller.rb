013557c23731c78650838ab78c1cf0324942d29b
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-04-17 14:24:50 +0900

    wday_numが7以上となるように条件式を修正した

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -37,7 +37,7 @@ class CalendarsController < ApplicationController
 
       wday_num = @todays_date.wday + x
 
-      if wday_num >= 6
+      if wday_num >= 7
         wday_num = wday_num - 7
       end
 
